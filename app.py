34bc88ab1a5575bb70c3c9e0fb03858f20f3ae04
lucas-satoshi <lucas.satoshi.18@gmail.com>
2024-08-29 11:20:17 -0300

    third commit

--- app.py
+++ app.py
@@ -6,17 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/area-logada')
-def area_logada():
-    return render_template('area-logada.html')
-
-@app.route('/cadastrar')
-def cadastrar():
-    return render_template('cadastrar.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
